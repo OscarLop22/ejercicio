7fd62e00cc560eb0abbe10ffa5a158e9a5976dda
OscarLop22 <151667023+OscarLop22@users.noreply.github.com>
2024-03-04 17:13:01 +0100

    version_3

--- git.py
+++ git.py
@@ -1,6 +1,9 @@
-list = [3, 4, 5, 6, 1, 2, 5, 9]
-max = max(list)
-min = min(list)
 
-print(max)
-print(min)
+def maximo():
+    list = [3, 4, 5, 6, 1, 2, 5, 9]
+    max = max(list)
+
+    print(max)
+
+
+print(maximo)
